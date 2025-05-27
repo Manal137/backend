50f86ac6290056bc89bce8fdce823302b380ff71
Manal137 <manal.saheer@biztras.com>
2025-05-27 16:25:49 +0530

    mmnn

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ app.use('/api/auth', require('./routes/auth')); // ✅ Correct
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Backend is running.');
+});
